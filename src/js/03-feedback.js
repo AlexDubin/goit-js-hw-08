ce8a01b69376e290d2eddf77292a2f3dbd31d71b
Oleksandr Dubin <102242242+AlexDubin@users.noreply.github.com>
2023-04-28 19:21:09 +0200

    Изменения в файле index.html

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash/throttle';
 
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const saveState = throttle(() => {
+  const state = { email: emailInput.value, message: messageInput.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(state));
+}, 500);
+
+const handleInput = () => {
+  saveState();
+};
+
+const restoreState = () => {
+  const state = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  emailInput.value = state.email || '';
+  messageInput.value = state.message || '';
+};
+
+const handleSubmit = event => {
+  event.preventDefault();
+  const state = { email: emailInput.value, message: messageInput.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(state));
+  emailInput.value = '';
+  messageInput.value = '';
+  localStorage.removeItem('feedback-form-state');
+  console.log(state);
+};
+
+form.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
+
+restoreState();
